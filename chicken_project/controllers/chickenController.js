90b042fe647873df93e3a4fe00c6280789ebde68
LounesBrahimi <lounes.brahimi@etu.sorbonne-universite.fr>
2022-01-06 02:17:30 +0100

    UPDATE a chicken already in the dataBase at localhost://5500/chickens/id successfully

--- chicken_project/controllers/chickenController.js
+++ chicken_project/controllers/chickenController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { ChickenModel } = require('../models/chickenModel');
+const ObjectID = require('mongoose').Types.ObjectId;
 
 
 // Get the list of all chickens stocked in chicken_db
@@ -26,6 +27,30 @@ router.post('/', (req, res) => {
       if (!err) res.send(docs);
       else console.log('Error creating new data : ' + err);
     })
+  });
+
+  // UPDATE a new chicken to the chicken_db
+  router.put("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknow : " + req.params.id)
+    
+    const updateRecord = {
+      name: req.body.name,
+      birthday: req.body.birthday,
+      weight: req.body.weight,
+      steps: req.body.steps,
+      isRunning: req.body.isRunning
+    };
+  
+    ChickenModel.findByIdAndUpdate(
+      req.params.id,
+      { $set: updateRecord},
+      { new: true },
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Update error : " + err);
+      }
+    )
   });
 
   module.exports = router;
