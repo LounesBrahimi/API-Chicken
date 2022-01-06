8b2d9c6c9bc5ecc7616a84b66f5623b36e1919e3
LounesBrahimi <lounes.brahimi@etu.sorbonne-universite.fr>
2022-01-06 08:22:50 +0100

    Make the project universal

--- chicken_project/index.js
+++ chicken_project/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
 require('./models/dataBaseConfigs');
 const bodyParser = require('body-parser');
 
 const chickenRoutes = require('./controllers/chickenController');
 
 app.use(bodyParser.json());
+app.use(cors({origin : 'https://cdpn.io'}));
 app.use('/chickens', chickenRoutes);
 app.use('/chickens/run', chickenRoutes);
 
