d7199ebdc0f99c910537050e8d5b2722dddae213
LounesBrahimi <lounes.brahimi@etu.sorbonne-universite.fr>
2022-01-06 02:19:59 +0100

    DELETE a chicken already in the dataBase at localhost://5500/chickens/id successfully

--- chicken_project/controllers/chickenController.js
+++ chicken_project/controllers/chickenController.js
@@ -53,4 +53,17 @@ router.post('/', (req, res) => {
     )
   });
 
+  // DELETE a chicken from the chicken_db
+  router.delete("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknow : " + req.params.id)
+    
+    ChickenModel.findByIdAndRemove(
+      req.params.id,
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Delete error : " + err);
+      })
+  });
+
   module.exports = router;
